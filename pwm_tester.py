7e2113584b842be8d42c9b8d57832e8432bfa6e9
valex2 <92407277+valex2@users.noreply.github.com>
2024-05-17 00:54:43 -0700

    Update pwm_tester.py

--- pwm_tester.py
+++ pwm_tester.py
@@ -7,7 +7,29 @@ def main():
 
     for j in range(2):
         if j == 1:
-            time.sleep(5)
+            time.sleep(2) 
+            command = "voltage\n"
+            print(command)
+            portName.write(command.encode())
+            data = portName.read(portName.in_waiting)
+            print(data.decode('utf-8'))
+
+            time.sleep(2) 
+            command = "depth\n"
+            print(command)
+            portName.write(command.encode())
+            data = portName.read(portName.in_waiting)
+            print(data.decode('utf-8'))
+            
+            time.sleep(2) 
+            command = "voltage\n"
+            print(command)
+            portName.write(command.encode())
+            data = portName.read(portName.in_waiting)
+            print(data.decode('utf-8'))
+            
+            time.sleep(2)
+
         for i, thruster in enumerate(thruster_ids):
             if j == 0:
                 command = f"{i+2} 1600\n"
@@ -16,6 +38,25 @@ def main():
             print(f"command: {command}")
             portName.write(command.encode())
             ##time.sleep(.1)
+
+    time.sleep(2)
+    # test different thrusts
+    command = "2 1600\n"
+    print(command)
+    portName.write(command.encode())
+    # 3 should not go
+    time.sleep(3)
+    command = "3 1600\n"
+    print(command)
+    portName.write(command.encode())
+    command = "2 1500\n"
+    print(command)
+    portName.write(command.encode())
+    # swap 
+    time.sleep(3)
+    command = "3 1500\n"
+    print(command)
+    portName.write(command.encode())
 
 if __name__ == '__main__':
     main()
